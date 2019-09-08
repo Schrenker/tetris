f0b503300ba830fb15ce13a148115ee84679351e
schrenker <zawadzkis95@gmail.com>
2019-09-08 22:32:29 +0200

    set up of screen, window and constants

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import curses
+import consts as const
 
 
 def main(stdscr):
-    pass
+
+    #curses init
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+
+    stdscr.nodelay(True)
+
+    gameWindow = curses.newwin(const.HEIGHT, const.WIDTH, 2, 2)
+    gameWindow.keypad(1)
+    gameWindow.timeout(const.TIMEOUT)
 
 
 if __name__ == "__main__":
