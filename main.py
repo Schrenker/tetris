1d2d4ba510970826d995995d2463c4c211353c78
schrenker <zawadzkis95@gmail.com>
2019-09-08 22:46:20 +0200

    basic game loop done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import curses
+from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 import consts as const
 
 
@@ -11,10 +12,36 @@ def main(stdscr):
 
     stdscr.nodelay(True)
 
-    gameWindow = curses.newwin(const.HEIGHT, const.WIDTH, 2, 2)
-    gameWindow.keypad(1)
-    gameWindow.timeout(const.TIMEOUT)
+    #game window init
+    game_window = curses.newwin(const.HEIGHT, const.WIDTH, 2, 2)
+    game_window.keypad(1)
+    game_window.timeout(const.TIMEOUT)
 
+    # variables init
+    key = KEY_UP
+    fall_counter = 0
+
+    # loop init
+    while key is not ord('q'):
+
+        game_window.box()
+
+        next_key = game_window.getch()
+        key = key if next_key == -1 else next_key
+
+        moves = {
+            KEY_DOWN: "down",
+            KEY_LEFT: "left",
+            KEY_RIGHT: "right"
+        }
+
+        if key in moves:
+            moves[key]()
+
+        key = KEY_UP
+
+
+        stdscr.refresh()
 
 if __name__ == "__main__":
     curses.wrapper(main)
